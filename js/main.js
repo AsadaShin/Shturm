8e90f31e79154956aa9eca635b72679226281bb4
AsadaShin <Ivan.Fedoseev1941@gmail.com>
2022-09-24 19:34:13 +0300

    add portfolio section and slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.portfolio-slider__list').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
